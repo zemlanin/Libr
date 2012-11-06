335b3b74bf397e93f5d0d48eccb786199a2947a6
Anton Vérinov <github@zemlan.in>
2012-11-06 20:50:26 +0200

    Обработка форм wtforms

---
+++ forms.py
@@ -0,0 +1,11 @@
+from wtforms import Form, BooleanField, TextField, SelectMultipleField, \
+    SubmitField, validators
+
+class AuthorForm(Form):
+    name = TextField('Name')
+
+class BookForm(Form):
+    title = TextField('Title')
+
+class AuthorshipForm(Form):
+    books = SelectMultipleField('Authors', coerce=int)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 from flask import Flask, render_template, request, redirect, url_for
-from wtforms import form
+from forms import BookForm, AuthorForm, AuthorshipForm
 from database import db_session, init_db
 from models import Book, Author
 
@@ -17,15 +17,20 @@ def shutdown_session(exception=None):
 def index():
     books = Book.query.all()
     authors = Author.query.all()
+    forms = {'book': BookForm(), 'author': AuthorForm()}
     page = {'title': 'Libr'}
     return render_template("index.html", **locals())
 
 @app.route("/author/add", methods=["POST"])
 def addauthor():
-    a = Author(request.form["author_name"])
-    db_session.add(a)
-    db_session.commit()
-    return redirect(url_for("singleauthor", a_id=a.id))
+    form = AuthorForm(request.form)
+    if form.validate():
+        a = Author(form.name.data)
+        db_session.add(a)
+        db_session.commit()
+        return redirect(url_for("singleauthor", a_id=a.id))
+    else:
+        return redirect(url_for("index"))
 
 @app.route("/author/<int:a_id>", methods=["GET", "POST", "DELETE"])
 def singleauthor(a_id):
@@ -33,11 +38,17 @@ def singleauthor(a_id):
     if request.method == "GET":
         books = author.books
         page = {'title': author.name}
+        forms = {
+            'author': AuthorForm(),
+            'book': BookForm(),
+            'authorship': AuthorshipForm()
+        }
+        forms['authorship'].books.choices = [(b.id, b.title) for b in Book.query.all()]
         return render_template("author.html", **locals())
     elif request.method == "POST":
-        author.name = request.form["author_name"]
+        author.name = request.form["name"]
         db_session.commit()
-        return redirect(url_for("singleauthor", a_id=a_id))
+        return redirect(url_for("singleauthor", a_id=a_id)) # либо рендерить?..
     elif request.method == "DELETE":
         db_session.delete(author)
         db_session.commit()
@@ -45,7 +56,7 @@ def singleauthor(a_id):
 
 @app.route("/book/add", methods=["POST"])
 def addbook():
-    b = Book(request.form["book_title"])
+    b = Book(request.form["title"])
     db_session.add(b)
     db_session.commit()
     return redirect(url_for("singlebook", b_id=b.id))
@@ -56,9 +67,10 @@ def singlebook(b_id):
     if request.method == "GET":
         authors = book.authors
         page = {'title': book.title}
+        forms = {'book': BookForm()}
         return render_template("book.html", **locals())
     elif request.method == "POST":
-        book.title = request.form["book_title"]
+        book.title = request.form["title"]
         db_session.commit()
         return redirect(url_for("singlebook", b_id=b_id))
     elif request.method == "DELETE":
@@ -66,17 +78,23 @@ def singlebook(b_id):
         db_session.commit()
         return url_for("index")
 
-@app.route("/signbook/<int:a_id>/<int:b_id>", methods=["PUT", "DELETE"])
-def signbook(a_id, b_id):
+@app.route("/author/<int:a_id>/sign", methods=["POST"])
+def signbook(a_id):
     author = Author.query.filter(Author.id == a_id).one()
-    if request.method == "PUT":
-        author.books.append(Book.query.filter(Book.id == b_id).one())
-        db_session.commit()
-        return url_for("singleauthor", a_id=author.id)
-    elif request.method == "DELETE":
-        author.books.remove(Book.query.filter(Book.id == b_id).one())
-        db_session.commit()
-        return url_for("index")
+    b_ids = request.form.getlist('books')
+    books = Book.query.filter(Book.id.in_(b_ids)).all()
+    for b in books:
+        author.books.append(b)
+    db_session.commit()
+    return redirect(url_for("singleauthor", a_id=a_id))
+
+# Да, лучше бы было DELETE, но форму с ним не засабмитить нормально
+@app.route("/author/<int:a_id>/unsign/<int:b_id>", methods=["POST"])
+def unsignbook(a_id, b_id):
+    author = Author.query.filter(Author.id == a_id).one()
+    author.books.remove(Book.query.filter(Book.id == b_id).one())
+    db_session.commit()
+    return url_for("singleauthor", a_id=a_id)
 
 if __name__ == "__main__":
     app.run()
